0bc3f8c1d1e4aad997a16bb6d26020de69ce7514
PLDDev <wildmagicactive@gmail.com>
2023-03-27 17:09:03 +0300

    Выполнил базовую задачу 3-го спринта

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter = 0
+    
+    @IBOutlet weak var countButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        counterLabel.text = "0"
+        countButton.setTitle("Плюсани", for: .normal)
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedButton(_ sender: Any) {
+        counter += 1
+        counterLabel.text = String(counter)
+    }
+    
 }
 
